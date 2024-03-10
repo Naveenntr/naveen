ac71503886a906c82b36ef0ff703454280af031f
Naveenntr <kumarnaveenk7@gmail.com>
2024-03-10 20:30:46 +0530

    problem1

---
+++ Assignment_1/problem1.js
@@ -0,0 +1,17 @@
+let n=5
+for (let i=1;i<=n;i++)
+{
+  let row='';
+    for (j=1;j<=n*2-1;j++)
+    {
+      if (j>=n-(i-1) && j<=n+(i-1))
+      {
+        row+='*';
+      }
+      else 
+      {
+        row+=' ';
+      }
+    }
+      console.log(row)
+    }
