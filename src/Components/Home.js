a60fafc40c870b0a2cb7047a696a7cca19f31c5b
Naveenntr <kumarnaveenk7@gmail.com>
2024-07-28 10:42:29 +0530

    notfiundfixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <div>
           <h1 className="text-3xl font-semibold"   > Welcome to my page</h1><br /> <br />
           <p className="text-2xl">Hii...<br />
-            This is Naveen <br />
+            This is Naveen <br/>
             i am a website cordinator <br />
              Do you want to know more about me then please go to about page 
           </p>
